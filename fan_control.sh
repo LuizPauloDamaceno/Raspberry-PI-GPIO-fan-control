b88779cfc825f1c6815faff931ccc1cdaeab3824
electronicslover <luizpaulodamaceno@live.com>
2017-06-07 19:15:03 -0300

    Update values
    reduce fan spin delay and temperature limit for colder raspberry operation

--- fan_control.sh
+++ fan_control.sh
@@ -8,10 +8,10 @@ do
 hwtemp=$(cat /sys/class/thermal/thermal_zone0/temp) #reads actual temperature
 hwtemp=$((hwtemp/1000)) #temperature reading is in mCelcius, so we need to converte to celcius.
 
-if [ "$hwtemp" -ge "56" ]; then #send a high GPIO state if CPU temperature is higher than 56ºC
+if [ "$hwtemp" -ge "45" ]; then #send a high GPIO state if CPU temperature is higher than 56ºC
 echo 1 > /sys/class/gpio/gpio21/value #high GPIO state
 else #lower than 56, disables the fan.
 echo 0 > /sys/class/gpio/gpio21/value #low GPIO state
 fi
-sleep 0.100 #sleep 100mS
+sleep 0.005 #sleep 100mS
 done
